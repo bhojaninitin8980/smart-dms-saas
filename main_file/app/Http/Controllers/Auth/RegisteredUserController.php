de818aba26070781bc04786a2b379e70c5a43b16
Bhojani Nitin <92044682+bhojaninitin8980@users.noreply.github.com>
2023-11-13 14:01:24 +0530

    13 Nov 2023

--- main_file/app/Http/Controllers/Auth/RegisteredUserController.php
+++ main_file/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'type' => 'admin',
+            'subscription' => 1,
             'lang' => 'english',
         ]);
 
--- main_file/app/Http/Controllers/HomeController.php
+++ main_file/app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
                 header('location:install');
                 die;
             } else {
-                return redirect()->route('login');
+                return view('layouts.landing');
 
             }
 
